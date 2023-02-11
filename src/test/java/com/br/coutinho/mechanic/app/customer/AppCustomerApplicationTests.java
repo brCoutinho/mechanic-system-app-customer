ba855b3195d0e5f000b1a09f6fec2760912199a6
brCoutinho <thcoutinho1997@gmail.com>
2023-02-11 19:51:18 -0300

    first commit

---
+++ src/test/java/com/br/coutinho/mechanic/app/customer/AppCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.coutinho.mechanic.app.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
